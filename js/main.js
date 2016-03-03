294c97d4a806e8d60b5e2913c594b4960e102b54
PEsteves8 <pauloesteves8@gmail.com>
2016-03-03 17:26:19 +0000

    Add marker/list onclick features: toggle infoWindow and location highlight in list, marker bounces and camera pans to location on infowindow open

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// TODO: Correct insisible part of list || Make list smaller with smaller screens || Add checkboxes ||
+
 //List of locations
 var locations = [{
   name: "Café Piolho",
@@ -34,21 +36,23 @@ var Location = function(data) {
   this.name = ko.observable(data.name);
   this.lat = ko.observable(data.lat);
   this.lng = ko.observable(data.lng);
-
+  this.isVisible = ko.observable(true);
+  this.infoOpen = ko.observable(false);
 };
 
 var ViewModel = function(map) {
   var self = this;
-  var markers = [];
+
   self.googleMap = map;
 
+  //Create a list of locations from the locations array
   this.locationList = ko.observableArray([]);
 
   locations.forEach(function(loc) {
     self.locationList.push(new Location(loc));
   });
 
-
+  //Add a marker property to each location in locationList
   self.locationList().forEach(function(location) {
     location.marker = new google.maps.Marker({
       position: {
@@ -58,39 +62,63 @@ var ViewModel = function(map) {
       map: self.googleMap,
       title: location.name()
     });
-  })
+    //Create an infoWindow for each marker
+    var infoWindow = new google.maps.InfoWindow({
+      content: "<h1>" + location.name() + "</h1>"
+    });
+    //Add a short bounce animation and infoWindow on click on each marker
 
 
-  this.removeMarkers = function() {
-    self.locationList().forEach(function(location) {
-      location.marker.setMap(null);
+    location.toggleWindowOnClick = function() {
+
+        //Toogle info window open (there are two conditions because on first click .getMap() doesn't return null yet)
+
+        if (infoWindow.getMap() === null || typeof infoWindow.getMap() === "undefined") {
+          location.marker.setAnimation(google.maps.Animation.BOUNCE);
+          setTimeout(function() {
+            location.marker.setAnimation(null)
+          }, 750);
+
+          infoWindow.open(self.googleMap, location.marker);
+          self.googleMap.panTo(location.marker.getPosition());
+          location.infoOpen(true);
+
+        } else {
+          infoWindow.close();
+          location.infoOpen(false);
+        }
+
+      }
+      //Add event listener to run infowindow function on marker click
+    location.marker.addListener('click', location.toggleWindowOnClick);
+    //Add event listener to infoWindo close button so that the highlight in the locations list toggles
+    google.maps.event.addListener(infoWindow, 'closeclick', function() {
+      location.infoOpen(false);
     })
-  }
+  });
 
-
-
-  console.log(self.locationList()[0].marker);
-
+  //Value for the search bar
   this.queryValue = ko.observable("");
 
+  /*Function that runs each time a key is pressed in the search bar. It checks if the input
+    is part of any of the locations name and filters everything else*/
   this.search = function(value) {
 
-    for (var i = 0; i < locations.length; i++) {
-      if (locations[i].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
-        if (self.locationList()[i].marker.map === null) {
+    for (var i = 0; i < self.locationList().length; i++) {
+      if (self.locationList()[i].name().toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+        if (self.locationList()[i].isVisible() === false) {
           self.locationList()[i].marker.setMap(self.googleMap);
-          self.locationList()[i].name(locations[i].name);
+          self.locationList()[i].isVisible(true);
         }
 
       } else {
         self.locationList()[i].marker.setMap(null);
-        self.locationList()[i].name(null);
+        self.locationList()[i].isVisible(false);
       }
     }
-
   }
 
-
+  //Runs the search function everytime the query value changes
   this.queryValue.subscribe(this.search);
 
 }
@@ -107,6 +135,7 @@ function initMap() {
 
 };
 
+// Apply bindings with the map
 $(window).load(function() {
 
   var googleMap = initMap();
